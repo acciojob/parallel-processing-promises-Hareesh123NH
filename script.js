b6e57db6d172c01c6086b407407c2318d256ade1
Hareesh123NH <162862005+Hareesh123NH@users.noreply.github.com>
2024-10-09 15:58:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,32 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImg(image) {
+	return new Promise((res,rej)=>{
+		const img=new Image();
+		img.src=image.url;
+
+		img.onload=()=>res(img);
+		img.onerror=()=>rej(new Error(`Failed to load image's URL: ${image.url}`));
+		
+	});
+}
+
+function downloadAllImgs() {
+	const promises=images.map(img=>downloadImg(img));
+
+	Promise.all(promises)
+		.then((imgs)=>{
+			output.innerHTML='';
+			imgs.forEach(img => output.appendChild(img));
+		})
+		.catch(error => {
+            console.error(error.message);
+            alert(error.message);
+        });
+}
+
+btn.addEventListener('click', () => {
+    downloadAllImgs();
+});
